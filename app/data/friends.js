371000ccec414f1f2ad2d4bb616062bb8b3c506b
code4GG <cavallaro.av@gmail.com>
2018-03-07 17:57:41 -0500

    updated db

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,14 @@
 const friendsArr = [
- {
-    "name":"Ahmed",
-    "image":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "image":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
+     {
+        "name": "Snoopy",
+        "image": "https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Snoopy_Peanuts.png/200px-Snoopy_Peanuts.png",
+        "total": "10"
+    },
+    {
+        "name": "Woodstock",
+        "image": "https://vignette.wikia.nocookie.net/peanuts/images/e/eb/Woodstock5.jpg/revision/latest?cb=20160508092305",
+        "total": "11"
+    }
 ];
 
 module.exports = friendsArr;
